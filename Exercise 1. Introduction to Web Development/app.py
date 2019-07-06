4a74d65893700c23e2b34f23a234a1a947e9fc22
XCifer <13607960126@139.com>
2019-07-06 21:03:38 +0800

    file name

--- Exercise 1. Introduction to Web Development/app.py
+++
@@ -1,6 +0,0 @@
-from flask import Flask
-app = Flask("hello")
-
-@app.route("/")
-def index():
-    return "You expected this to say hello, but it says \"donkey swings\" instead. Who would have guessed?"
